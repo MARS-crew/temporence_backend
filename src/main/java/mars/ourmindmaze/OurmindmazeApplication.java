2fd9fd1091c9df1745e8e995719bc75f787613a4
pinomaker-hoo <inhoo23@naver.com>
2023-04-10 22:28:05 +0900

    Init : Create Project

---
+++ src/main/java/mars/ourmindmaze/OurmindmazeApplication.java
@@ -0,0 +1,13 @@
+package mars.ourmindmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurmindmazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurmindmazeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mars/ourmindmaze/OurmindmazeApplicationTests.java
@@ -0,0 +1,13 @@
+package mars.ourmindmaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurmindmazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
