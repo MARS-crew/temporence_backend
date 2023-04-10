a8e98a7b686d9a8620a09b78dbb0623f6777aea4
pinomaker-hoo <inhoo23@naver.com>
2023-04-10 22:32:37 +0900

    feat : Set Swagger Config

---
+++ src/main/java/mars/ourmindmaze/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package mars.ourmindmaze.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Mars Team 기가")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Mars Game Api")
+                        .description("Mars Api doc")
+                        .version("v0.0.1"));
+    }
+}
