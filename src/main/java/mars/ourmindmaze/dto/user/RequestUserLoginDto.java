d4482b180eb885d1956a3c61438109275171d48f
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-26 15:08:12 +0900

    fix : Add Example value of User Dto

--- src/main/java/mars/ourmindmaze/dto/user/RequestUserLoginDto.java
+++ src/main/java/mars/ourmindmaze/dto/user/RequestUserLoginDto.java
@@ -1,11 +1,15 @@
 package mars.ourmindmaze.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Data
 public class RequestUserLoginDto {
+    @Schema(example = "admimn")
     private String email;
+
+    @Schema(example = "1234")
     private String password;
     public UsernamePasswordAuthenticationToken toAuthentication( ){
         return new UsernamePasswordAuthenticationToken(email, password);
--- src/main/java/mars/ourmindmaze/dto/user/RequestUserSaveDto.java
+++ src/main/java/mars/ourmindmaze/dto/user/RequestUserSaveDto.java
@@ -1,10 +1,16 @@
 package mars.ourmindmaze.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RequestUserSaveDto {
+    @Schema(example = "admin@admin.com")
     private String email;
+
+    @Schema(example = "1234")
     private String password;
+
+    @Schema(example = "사용자")
     private String name;
 }
