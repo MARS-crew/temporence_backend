52c3c0e50cc61bc605344d507ca08cd0e3e1c5c4
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-28 12:18:14 +0900

    feat : FCM

---
+++ src/main/java/mars/ourmindmaze/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package mars.ourmindmaze.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance();
+    }
+}
