0bbb6b66fcd43be9258f7f273b06e36c243c71f5
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-28 23:29:09 +0900

    feat : Add Save DM

--- src/main/java/mars/ourmindmaze/controller/DmController.java
+++ src/main/java/mars/ourmindmaze/controller/DmController.java
@@ -1,9 +1,13 @@
 package mars.ourmindmaze.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mars.ourmindmaze.dto.dm.RequestDmSaveDto;
 import mars.ourmindmaze.service.DmService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -13,4 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "DM API", description = "메세지 API")
 public class DmController {
     private final DmService dmService;
+
+    @Operation(summary = "Send Message", description = "메세지 보내기")
+    @ApiResponse(responseCode = "400", description = "Parameter type is incorrect")
+    @ApiResponse(responseCode = "401", description = "Bad Credentials, JWT token expires")
+    @ApiResponse(responseCode = "401", description = "Access denied")
+    @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    @PostMapping
+    public ResponseEntity<?> saveFriend(@RequestBody RequestDmSaveDto dto) {
+        return dmService.saveDm(dto);
+    }
 }
--- src/main/java/mars/ourmindmaze/service/impl/DmServiceImpl.java
+++ src/main/java/mars/ourmindmaze/service/impl/DmServiceImpl.java
@@ -1,12 +1,38 @@
 package mars.ourmindmaze.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import mars.ourmindmaze.common.dto.ApiResponse;
+import mars.ourmindmaze.common.dto.CommonResponse;
+import mars.ourmindmaze.domain.DM;
+import mars.ourmindmaze.domain.User;
+import mars.ourmindmaze.dto.dm.RequestDmSaveDto;
+import mars.ourmindmaze.enums.ExceptionEnum;
 import mars.ourmindmaze.repository.dm.DmJpaRepository;
+import mars.ourmindmaze.repository.user.UserJpaRepository;
 import mars.ourmindmaze.service.DmService;
+import mars.ourmindmaze.util.SecurityUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class DmServiceImpl implements DmService {
     private final DmJpaRepository dmJpaRepository;
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public ResponseEntity<?> saveDm(RequestDmSaveDto dto) {
+        User loginUser = SecurityUtil.getCurrentUserId(userJpaRepository);
+
+        Optional<User> reciver = userJpaRepository.findById(dto.getReciverId());
+        if (reciver.isEmpty()) {
+            return ApiResponse.<Object>builder().ApiResponseBuilder(ExceptionEnum.NOT_FOUDN_USER).buildObject();
+        }
+        dmJpaRepository.save(DM.builder().reciver(reciver.get()).sender(loginUser).content(dto.getContent()).build());
+
+        return CommonResponse.createResponseMessage(HttpStatus.CREATED.value(), "메세지를 보냈습니다.");
+    }
 }
