9646db7148fc82e1ea050c82cd25ddc81484949f
pinomaker-hoo <inhoo23@naver.com>
2023-06-04 22:38:26 +0900

    test : cicd

--- git-dev-deploy.sh
+++ git-dev-deploy.sh
@@ -6,12 +6,11 @@ PID=$!
 wait $PID
 
 REPOSITORY=/home/ubuntu/temp/ourmindmaze-api
-scp -i ./seoul.pem ./build/libs/ourmindmaze-0.0.1-SNAPSHOT.jar ubuntu@43.200.204.186:$REPOSITORY/build/libs/ &
+scp -i seoul.pem ./build/libs/ourmindmaze-0.0.1-SNAPSHOT.jar ubuntu@43.200.204.186:$REPOSITORY/build/libs/ &
 wait
 
-
 echo "deploy on dev server"
-ssh -i ./seoul.pem ubuntu@43.200.204.186 "bash -s" < ./run-server.sh &
+ssh -i seoul.pem ubuntu@43.200.204.186 "bash -s" < ./run-server.sh &
 
 echo "Done"
 sudo gradle clean
