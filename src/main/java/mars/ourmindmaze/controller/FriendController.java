ed866b7efeb6cb9b0448d824e9987325dcd66d9f
pinomaker-hoo <inhoo23@naver.com>
2023-06-04 20:57:57 +0900

    fix : Friend Controller Swagger

--- src/main/java/mars/ourmindmaze/controller/FriendController.java
+++ src/main/java/mars/ourmindmaze/controller/FriendController.java
@@ -1,10 +1,14 @@
 package mars.ourmindmaze.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mars.ourmindmaze.common.dto.SwaggerConfig;
 import mars.ourmindmaze.dto.friend.RequestFriendSaveDto;
 import mars.ourmindmaze.service.FriendService;
 import org.springframework.http.ResponseEntity;
@@ -19,30 +23,33 @@ public class FriendController {
     private final FriendService friendService;
 
     @Operation(summary = "Save Friend", description = "친구 생성")
-    @ApiResponse(responseCode = "400", description = "Parameter type is incorrect")
-    @ApiResponse(responseCode = "401", description = "Bad Credentials, JWT token expires")
-    @ApiResponse(responseCode = "401", description = "Access denied")
-    @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "친구 생성에 성공하였습니다.", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.FRIEND_SAVE_RESPONSE))),
+            @ApiResponse(responseCode = "400", description = SwaggerConfig.BAD_REQUEST, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.BAD_REQUEST_RESPONSE)})),
+            @ApiResponse(responseCode = "401", description = SwaggerConfig.UNAUTHORIZED_ERROR, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.UNAUTHORIZED_ERROR_RESPONSE)})),
+            @ApiResponse(responseCode = "500", description = SwaggerConfig.INTERNAL_SERVER_ERROR, content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.INTERNAL_SERVER_ERROR_REPONSE)))})
     @PostMapping
     public ResponseEntity<?> saveFriend(@RequestBody RequestFriendSaveDto dto) {
         return friendService.saveFriend(dto);
     }
 
     @Operation(summary = "Find Friend List", description = "친구 리스트 조회")
-    @ApiResponse(responseCode = "400", description = "Parameter type is incorrect")
-    @ApiResponse(responseCode = "401", description = "Bad Credentials, JWT token expires")
-    @ApiResponse(responseCode = "401", description = "Access denied")
-    @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "포인트 조회에 성공하였습니다.", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.FRIEND_FIND_RESPONSE))),
+            @ApiResponse(responseCode = "400", description = SwaggerConfig.BAD_REQUEST, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.BAD_REQUEST_RESPONSE)})),
+            @ApiResponse(responseCode = "401", description = SwaggerConfig.UNAUTHORIZED_ERROR, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.UNAUTHORIZED_ERROR_RESPONSE)})),
+            @ApiResponse(responseCode = "500", description = SwaggerConfig.INTERNAL_SERVER_ERROR, content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.INTERNAL_SERVER_ERROR_REPONSE)))})
     @GetMapping
     public ResponseEntity<?> findFriendList() {
         return friendService.findFriendList();
     }
 
     @Operation(summary = "Delete Friend", description = "친구 삭제")
-    @ApiResponse(responseCode = "400", description = "Parameter type is incorrect")
-    @ApiResponse(responseCode = "401", description = "Bad Credentials, JWT token expires")
-    @ApiResponse(responseCode = "401", description = "Access denied")
-    @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "친구 삭제에 성공하였습니다.", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.FRIEND_FIND_RESPONSE))),
+            @ApiResponse(responseCode = "400", description = SwaggerConfig.BAD_REQUEST, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.BAD_REQUEST_RESPONSE)})),
+            @ApiResponse(responseCode = "401", description = SwaggerConfig.UNAUTHORIZED_ERROR, content = @Content(mediaType = "application/json", examples = {@ExampleObject(value = SwaggerConfig.UNAUTHORIZED_ERROR_RESPONSE)})),
+            @ApiResponse(responseCode = "500", description = SwaggerConfig.INTERNAL_SERVER_ERROR, content = @Content(mediaType = "application/json", examples = @ExampleObject(value = SwaggerConfig.INTERNAL_SERVER_ERROR_REPONSE)))})
     @DeleteMapping("/{id}")
     public ResponseEntity<?> findFriendList(@PathVariable(name = "id") Long id) {
         return friendService.deleteFriend(id);
