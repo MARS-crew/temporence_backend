9cb4bed3f567b22ded3cd19da8988704255fbd35
pinomaker-hoo <inhoo23@naver.com>
2023-06-08 09:52:49 +0900

    fix : Dto Example Data

--- src/main/java/mars/ourmindmaze/dto/userSkin/RequestUserSkinSaveDto.java
+++ src/main/java/mars/ourmindmaze/dto/userSkin/RequestUserSkinSaveDto.java
@@ -1,8 +1,11 @@
 package mars.ourmindmaze.dto.userSkin;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RequestUserSkinSaveDto {
+
+    @Schema(example = "1")
     private Long skinId;
 }
