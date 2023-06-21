9f8b6746cb9b0818ffaebcbc214e67a015633527
pinomaker-hoo <inhoo23@naver.com>
2023-06-22 00:48:40 +0900

    del : Item 관련 코드

--- src/main/java/mars/ourmindmaze/repository/item/ItemCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package mars.ourmindmaze.repository.item;
-
-import mars.ourmindmaze.vo.ItemVO;
-
-import java.util.List;
-
-public interface ItemCustomRepository {
-    List<ItemVO> findItemList();
-}
