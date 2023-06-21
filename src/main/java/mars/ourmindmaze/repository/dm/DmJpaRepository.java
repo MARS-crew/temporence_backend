2b362d3f2c188a564064aacf80ef9537049b2d91
pinomaker-hoo <inhoo23@naver.com>
2023-06-22 00:51:33 +0900

    del : DM 관련 코드

--- src/main/java/mars/ourmindmaze/repository/dm/DmJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package mars.ourmindmaze.repository.dm;
-
-import mars.ourmindmaze.domain.DM;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DmJpaRepository extends JpaRepository<DM, Long>, DmCustomRepository {
-}
