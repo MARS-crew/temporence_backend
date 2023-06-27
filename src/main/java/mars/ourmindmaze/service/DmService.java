e3b0ac1bcff291f578530ec1cb7df598c4f8db35
rlawnaks <rlawnaks1234@naver.com>
2023-06-27 16:53:15 +0900

    feat: Add Dm Service, Repository

---
+++ src/main/java/mars/ourmindmaze/service/DmService.java
@@ -0,0 +1,10 @@
+package mars.ourmindmaze.service;
+
+import mars.ourmindmaze.dto.dm.RequestDmSaveDto;
+import org.springframework.http.ResponseEntity;
+
+public interface DmService {
+
+    ResponseEntity<?> sendDm(RequestDmSaveDto dto);
+
+}
