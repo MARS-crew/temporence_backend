bec039a4c181f6877b137563be47396d1c041d21
pinomaker-hoo <inhoo23@naver.com>
2023-08-08 12:29:39 +0900

    fix : Main Class name

--- src/test/java/mars/temporence/TemporenceApplicationTests.java
+++ src/test/java/mars/temporence/TemporenceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OurmindmazeApplicationTests {
+class TemporenceApplicationTests {
 
 	@Test
 	void contextLoads() {
