e65ccec107690aeebe45b4b478bd54bc0c4fbaf7
pinomaker-hoo <inhoo23@naver.com>
2023-08-23 09:51:38 +0900

    fix : Token 유효 시간

--- src/main/java/mars/temporence/jwt/TokenProvider.java
+++ src/main/java/mars/temporence/jwt/TokenProvider.java
@@ -29,8 +29,8 @@ public class TokenProvider implements InitializingBean {
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 180;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24;
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 180;
     private final String secret;
     private Key key;
 
@@ -55,6 +55,7 @@ public class TokenProvider implements InitializingBean {
         long now = (new Date()).getTime();
 
         Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpriseIn = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
 
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())
@@ -64,7 +65,7 @@ public class TokenProvider implements InitializingBean {
                 .compact();
 
         String refreshToken = Jwts.builder()
-                .setExpiration(new Date(now+REFRESH_TOKEN_EXPIRE_TIME))
+                .setExpiration(refreshTokenExpriseIn)
                 .signWith(key,SignatureAlgorithm.HS512)
                 .compact();
 
@@ -114,7 +115,8 @@ public class TokenProvider implements InitializingBean {
             logger.warn("잘못된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
             logger.warn("만료된 JWT 토큰입니다.");
-        } catch (UnsupportedJwtException e) {
+        }
+        catch (UnsupportedJwtException e) {
             logger.warn("지원되지 않는 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
             logger.warn("JWT 토큰이 잘못되었습니다.");
