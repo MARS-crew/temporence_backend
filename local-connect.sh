2d24e4c085f2174125da18cc359ef3dc7e309343
pinomaker-hoo <inhoo23@naver.com>
2023-08-23 09:49:45 +0900

    fix : 임시용 로컬 실행 shell

--- local-connect.sh
+++ local-connect.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh -L 3306:localhost:3307 -L 6379:localhost:6379 pinomaker
+#ssh -L 3306:localhost:3307 -L 6379:localhost:6379 pinomaker
+ssh -L 3306:localhost:3307 -L 6379:localhost:6379 john
